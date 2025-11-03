df92ed4867c79684a68d56a0438dc9b5420cd8d8
Hongpeng Yang <34737752+YYYYYYHP@users.noreply.github.com>
2025-11-02 22:01:21 -0500

    Update preprocessing_feature.py

--- preprocessing/preprocessing_feature.py
+++ preprocessing/preprocessing_feature.py
@@ -1,6 +1,6 @@
 import time
 import sys
-
+import pandas as pd
 sys.path.append('..')
 from source.analysis.setup.subject_builder import SubjectBuilder
 from source.constants import Constants
@@ -11,7 +11,7 @@ from joblib import Parallel, delayed
 
 def run_preprocessing(subject_set):
     start_time = time.time()
-    parallel = Parallel(n_jobs=-1)
+    parallel = Parallel(n_jobs=5)
     parallel(delayed(RawDataProcessor.crop_all)(str(subject_set[i])) for i in (range(len(subject_set))))
 
     parallel(delayed(FeatureBuilder.build)(str(subject_set[i])) for i in (range(len(subject_set))))
@@ -21,6 +21,5 @@ def run_preprocessing(subject_set):
 
 
 if __name__ == '__main__':
-    subject_ids = SubjectBuilder.get_all_subject_ids_from_path(Constants.PSG_FILE_PATH)
-
+    subject_ids = pd.read_csv(f'../data/{Constants.DEVICE}_ids.csv')['subject'].tolist()
     run_preprocessing(subject_ids)
